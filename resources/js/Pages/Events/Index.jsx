df6b79f7260698177346d48c5dae873c5711088a
ArturBraun <55237940+ArturBraun@users.noreply.github.com>
2022-10-30 13:13:16 +0100

    Added printing functionality

--- resources/js/Pages/Events/Index.jsx
+++ resources/js/Pages/Events/Index.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/inertia-react';
 import Event from '@/Components/Event';
-import NewEvent from '@/Components/NewEvent';
+import EventActions from '@/Components/EventActions';
  
 export default function Index({ auth, events }) { 
     return (
@@ -14,7 +14,7 @@ export default function Index({ auth, events }) {
  
             <div className="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
                 <ol className="relative border-l border-gray-200 dark:border-gray-700">  
-                    <NewEvent />
+                    <EventActions />
 
                     {events.map(event =>
                         <Event key={event.id} event={event} />
